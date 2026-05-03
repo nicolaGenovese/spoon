9638d30442f8597d7de5ee0f465c87218edbb3f6
Nicola Genovese <nicol.genovese@gmail.com>
2026-05-03 13:06:46 +0100

    database

--- cloud-config.js
+++ cloud-config.js
@@ -4,7 +4,7 @@
 // 3. Inserisci URL e anon key del progetto qui sotto.
 // 4. Metti enabled: true prima di pubblicare su GitHub Pages.
 window.SPOON_CLOUD_CONFIG = {
-  enabled: false,
+  enabled: true,
   provider: "supabase",
   supabaseUrl: "https://sudysoizwfddpladkzeo.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1ZHlzb2l6d2ZkZHBsYWRremVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3OTMwMjQsImV4cCI6MjA5MzM2OTAyNH0.hv-k4qvN7bel-vEA0kbqOiionEu1nMoe3QezUA6ZosI",
